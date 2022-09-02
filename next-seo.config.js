8aac396fe0a3447952c063b6c57070c75e496e7c
duclm <ducle7144@gmail.com>
2022-09-02 20:14:50 +0700

    chore: change site name

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,19 +7,15 @@ const defaultSEOConfig = {
   canonical: "https://rice-paper.vercel.app/",
   openGraph: {
     url: "https://rice-paper.vercel.app/",
-    title: "nextarter-chakra",
+    title: "Bánh tráng Hương Gió - Đặc sản Tây Ninh",
     description: "Đem đến những loại bánh tráng ngon nhất trên thị trường",
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
+        url: "/Ricepaperlogo.png",
         alt: "Bánh tráng Hương Gió og-image",
       },
     ],
     site_name: "Bánh tráng Hương Gió",
-  },
-  twitter: {
-    handle: "@sozonome",
-    cardType: "summary_large_image",
   },
 };
 
